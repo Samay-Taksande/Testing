2dbed32e91e8ea8b528014e44264757eee20225f
Samay Taksande <samaytaksande2510@gmail.com>
2025-12-11 12:02:54 +0530

    Done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,8 @@ export default function App() {
   return (
     <div className="h-20 w-20 bg-black text-white flex items-center justify-center">
       App
+
+      <h1>Samay Taksande </h1>
     </div>
   );
 }
